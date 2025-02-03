77b5ba3a3e46e8b5f6de7e7c360f00ba183e4288
aswanthup06 <aswanthup06@gmail.com>
2025-02-03 18:56:29 +0530

    9th

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let selectedDropdownValue = "";
+
 // Toggle Dropdown Visibility
 function toggleDropdown() {
     document.querySelector(".dropdown").classList.toggle("active");
@@ -6,8 +8,9 @@ function toggleDropdown() {
   // Select Option and Update Display
   function selectOption(value) {
     document.getElementById("selected-value").innerText = value;
+    selectedDropdownValue = value;
     document.querySelector(".dropdown").classList.remove("active");
-  }
+}
   
   // Close Dropdown When Clicking Outside
   document.addEventListener("click", function (event) {
@@ -15,4 +18,34 @@ function toggleDropdown() {
       document.querySelector(".dropdown").classList.remove("active");
     }
   });
+
+
+  function validateForm() {
+ 
+
+    let name = document.getElementById("name").value.trim();
+    let contact = document.getElementById("contact").value.trim();
+    let experience = document.getElementById("experience").value.trim();
+    let errorElement = document.getElementById("empty-error");
+    let successMessage = document.getElementById("success-message");
+    let submitButton = document.getElementById("submit-btn");
+
+    if (name === "" || contact === "" || experience === "" || selectedDropdownValue === "") {
+      errorElement.textContent = "All fields are mandatory";
+      errorElement.style.color = "red"; 
+      successMessage.style.display = "none";
+      return; 
+  }
+
+  errorElement.textContent = "";
+  successMessage.style.display = "block";
+  submitButton.style.display = "none";
+
+
+  console.log("Name:", name);
+  console.log("Contact:", contact);
+  console.log("Experience:", experience);
+  console.log("Dropdown Value:", selectedDropdownValue);
+}
+ 
   
